b8bc937810e83bd456cf971f9f019243595288f0
sptkl <caobaiyue@gmail.com>
2019-04-29 15:26:57 -0400

    add nycha_policeservices =

---
+++ sql/Untitled Query.sql
@@ -0,0 +1,49 @@
+
+--select w.status::text, count(*) 
+--from (select geo::json->'status' as status from dfta_contracts) w
+--group by w.status::text;
+
+ALTER TABLE nycha_policeservice
+	ADD hash text, 
+	ADD	facname text,
+	ADD	factype text,
+	ADD	facsubgrp text,
+	ADD	facgroup text,
+	ADD	facdomain text, 
+	ADD	servarea text,
+	ADD	opname text,
+	ADD	opabbrev text,
+	ADD	optype text,
+	ADD	overagency text,
+	ADD	overabbrev text,
+	ADD	overlevel text,
+	ADD	capacity text,
+	ADD	captype text,
+	ADD	proptype text;
+
+update nycha_policeservice as t
+SET hash =  md5(CAST((t.*)AS text)), 
+	facname = initcap(sponsor_name), 
+	factype = (CASE
+					WHEN (contract_type LIKE '%INNOVATIVE%' AND RIGHT(provider_id,2) <> '01') or
+						 (contract_type LIKE '%NEIGHBORHOOD%' AND RIGHT(provider_id,2) <> '01') THEN 'Satellite Senior Centers'
+					WHEN contract_type LIKE '%INNOVATIVE%' THEN 'Innovative Senior Centers'
+					WHEN contract_type LIKE '%NEIGHBORHOOD%' THEN 'Neighborhood Senior Centers'
+					WHEN contract_type LIKE '%MEALS%' THEN  initcap(contract_type)
+					ELSE 'Senior Services'
+				END),
+
+	facsubgrp = 'Senior Services',
+	facgroup = 'Human Services',
+	facdomain = 'Health and Human Services',
+	servarea = NULL,
+	opname = initcap(sponsor_name),
+	opabbrev = 'Non-public',
+	optype = 'Non-public',
+	overagency = 'NYC Department for the Aging',
+	overabbrev = 'NYCDFTA', 
+	overlevel = NULL, 
+	capacity = NULL, 
+	captype = NULL, 
+	proptype = NULL
+;
---
+++ sql/nycha_policeservice.sql
@@ -0,0 +1,41 @@
+
+--select w.status::text, count(*) 
+--from (select geo::json->'status' as status from dfta_contracts) w
+--group by w.status::text;
+
+ALTER TABLE nycha_policeservice
+	ADD hash text, 
+	ADD	facname text,
+	ADD	factype text,
+	ADD	facsubgrp text,
+	ADD	facgroup text,
+	ADD	facdomain text, 
+	ADD	servarea text,
+	ADD	opname text,
+	ADD	opabbrev text,
+	ADD	optype text,
+	ADD	overagency text,
+	ADD	overabbrev text,
+	ADD	overlevel text,
+	ADD	capacity text,
+	ADD	captype text,
+	ADD	proptype text;
+
+update nycha_policeservice as t
+SET hash =  md5(CAST((t.*)AS text)), 
+	facname = initcap(psa),
+	factype = 'NYCHA Police Service',
+	facsubgrp = 'Police Services',
+	facgroup = 'Public Safety',
+	facdomain = 'Public Safety, Emergency Services, and Administration of Justice',
+	servarea = NULL,
+	opname = 'New York City Housing Authority',
+	opabbrev = 'NYCHA',
+	optype = 'Public',
+	overagency = 'New York City Housing Authority',
+	overabbrev = 'NYCHA', 
+	overlevel = NULL, 
+	capacity = NULL, 
+	captype = NULL, 
+	proptype = NULL
+;
