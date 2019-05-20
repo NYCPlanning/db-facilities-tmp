dd2a24ba6c4a0df20f3cf0286ff7aeef5808e6d4
sptkl <caobaiyue@gmail.com>
2019-05-20 16:12:38 -0400

    update

--- lib/cli.py
+++ lib/cli.py
@@ -1,12 +1,12 @@
 import click
 import os 
 from pathlib import Path
-from lib.s3.make_client import make_client, get_last_modified
+from sqlalchemy import create_engine, text
 
-client = make_client()
-bucket = os.environ.get('BUCKET')
+etl_path = Path(__file__).parent.parent / 'etl'
+sql_path = Path(__file__).parent.parent / 'sql'
 
-recipe_path = Path(__file__).parent.parent / 'recipes'
+engine = create_engine(os.environ.get('DATAFLOWS_DB_ENGINE'))
 
 @click.group()
 def cli():
@@ -17,90 +17,68 @@ def recipe():
         pass
 
 def get_recipes(ctx, args, incomplete):
-        return [k for k in os.listdir(recipe_path) if incomplete in k]
+        return [k for k in os.listdir(etl_path) if incomplete in k]
 
 @recipe.command('ls')
 @click.argument('recipe', type=click.STRING, autocompletion=get_recipes)
 def list_recipes(recipe):
         if recipe == 'local':
-
                 click.echo('\n')
-                for i in os.listdir(recipe_path):
-                        click.secho(i, fg='yellow')
+                for i in os.listdir(etl_path):
+                        if i not in ['__init__.py', 'utils.py', 'test.py', 'template.py', '__pycache__']:
+                                click.secho(i, fg='yellow')
                 click.echo('\n')
 
-        elif recipe == 's3': 
-                objs = client.list_objects_v2(Bucket=bucket, Prefix='recipes').get('Contents')
-                try:
-                        recipes = set([Path(obj['Key']).parts[1] for obj in objs])
-                        click.echo('\n')
-                        for i in recipes:
-                                click.secho(i, fg='magenta')
-                        click.echo('\n')
-                except TypeError:
-                        click.secho(f'BUCKET {bucket} is empty', fg='red')
+        elif recipe == 'pg':
+                click.echo('\n')
+                for i in engine.table_names():
+                        if i not in ['spatial_ref_sys', 'facilities']:
+                                click.secho(i, fg='red')
+                click.echo('\n')
                         
         elif recipe == 'diff': 
-                objs = client.list_objects_v2(Bucket=bucket, Prefix='recipes').get('Contents')
                 try: 
-                        in_s3 = set([Path(obj['Key']).parts[1] for obj in objs])
-                        in_local = set(os.listdir(recipe_path))
-
+                        in_pg = set(set(engine.table_names()) - set(['spatial_ref_sys', 'facilities']))
+                        objs = set(set(os.listdir(etl_path)) - set(['__init__.py', 'utils.py', 'test.py', 'template.py', '__pycache__']))
+                        in_local = set([obj.split('.')[0] for obj in objs])
                         click.echo('\n')
-                        click.secho('in s3 not in local:', fg='green', bold=True)
-                        for i in in_s3.difference(in_local):
+                        click.secho('in postgres not in local:', fg='green', bold=True)
+                        for i in in_pg.difference(in_local):
                                 click.secho(i, fg='magenta')
                         click.echo('\n')
 
-                        click.secho('in local not in s3:', fg='green', bold=True)
-                        for i in in_local.difference(in_s3):
+                        click.secho('in local not in postgres:', fg='green', bold=True)
+                        for i in in_local.difference(in_pg):
                                 click.secho(i, fg='yellow')
                         click.echo('\n')
                 except TypeError:
-                        click.secho(f'BUCKET {bucket} is empty', fg='red') 
-        else:
-                try: 
-                        objs = client.list_objects_v2(Bucket=bucket, Prefix=f'recipes/{recipe}').get('Contents')
-                        versions = set([Path(obj['Key']).parts[2] \
-                                        for obj in sorted(objs, key=get_last_modified)])
-                        
-                        click.echo('\n')
-                        click.secho('versions:', fg='green', bold=True)
-                        for i in versions: 
-                                click.secho(i, fg='magenta')
-                        click.echo('\n')
-
-                except TypeError:
-                        click.echo('\n')
-                        click.secho(f'{recipe} is not present in S3', fg='red', bold=True)
-                        click.echo(f'Do you want to run {recipe}? [y/n]', nl=False)
-                        c = click.getchar()
-                        if c == 'y': 
-                                click.echo('\n')
-                                click.echo(f'running {recipe} ...')
-                                os.system(f'python {recipe_path/recipe}/build.py')
-                                click.echo('\n')
-                        elif c == 'n':
-                                click.echo('Ok bye ...')
-                        else:
-                                click.echo('Invalid input')
-
+                        click.secho(f'type error', fg='red') 
+       
 @recipe.command('run')
 @click.argument('recipe', type=click.STRING, autocompletion=get_recipes)
 def run_recipes(recipe):
-    os.system(f'python {recipe_path/recipe}/build.py')
+        click.echo('\n')
+        click.secho(f'Pulling {recipe} from s3 and loading to postgres ... ', fg='yellow')
+        os.system(f'python {etl_path}/{recipe}.py')
 
+        click.echo('\n')
+        click.secho(f'Perform transformation in postgres ... ', fg='yellow')
+        sql_file = open(sql_path/f'{recipe}.sql')
+        escaped_sql = text(sql_file.read())
+        engine.execute(escaped_sql)
 
-@cli.group()
-def pipeline():
-        pass
+        click.echo('\n')
+        if recipe in engine.table_names(): 
+                click.secho(f'{recipe} is COOKED!', fg='blue')
+        else: 
+                click.secho(f'something went wrong ...', fg='red')
+        click.echo('\n')
 
-pipeline_path = Path(__file__).parent.parent / 'pipelines'
+@recipe.command('init')
+def init_recipes():
+        click.secho(f'Initializing ... ', fg='yellow')
 
-def get_pipelines(ctx, args, incomplete):
-        return [k for k in os.listdir(pipeline_path) if incomplete in k]
-
-@pipeline.command('run')
-@click.argument('pipeline', type=click.STRING, autocompletion=get_pipelines)
-def run_pipeline(pipeline):
-    os.system(f'python {pipeline_path/pipeline}/build.py')
+        create = open(sql_path/'create.sql')
+        load_to_facilities = open(sql_path/'create.sql')
+        escaped_sql = text(sql_file.read())
+        engine.execute(escaped_sql)
