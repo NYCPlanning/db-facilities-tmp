43fef3c1ac2d78c25620ba7f8b0e4b9b2a947689
sptkl <caobaiyue@gmail.com>
2019-05-21 11:50:31 -0400

    update qpl_libraries.sql

--- sql/dpr_parksproperties.sql
+++ sql/dpr_parksproperties.sql
@@ -4,21 +4,21 @@
 
 ALTER TABLE dpr_parksproperties
 	ADD hash text,
-	ADD	facname text,
-	ADD	factype text,
-	ADD	facsubgrp text,
-	ADD	facgroup text,
-	ADD	facdomain text, 
-	ADD	servarea text,
-	ADD	opname text,
-	ADD	opabbrev text,
-	ADD	optype text,
-	ADD	overagency text,
-	ADD	overabbrev text,
-	ADD	overlevel text,
-	ADD	capacity text,
-	ADD	captype text,
-	ADD	proptype text;
+	ADD facname text,
+	ADD factype text,
+	ADD facsubgrp text,
+	ADD facgroup text,
+	ADD facdomain text, 
+	ADD servarea text,
+	ADD opname text,
+	ADD opabbrev text,
+	ADD optype text,
+	ADD overagency text,
+	ADD overabbrev text,
+	ADD overlevel text,
+	ADD capacity text,
+	ADD captype text,
+	ADD proptype text;
 
 UPDATE dpr_parksproperties as t
 SET hash = md5(CAST((t.*)AS text)),
--- sql/qpl_libraries.sql
+++ sql/qpl_libraries.sql
@@ -4,38 +4,43 @@ from (select geo::json->'status' as status from qpl_libraries) w
 group by w.status::text;
 
 ALTER TABLE qpl_libraries
-	ADD hash text, 
-	ADD	facname text,
-	ADD	factype text,
-	ADD	facsubgrp text,
-	ADD	facgroup text,
-	ADD	facdomain text, 
-	ADD	servarea text,
-	ADD	opname text,
-	ADD	opabbrev text,
-	ADD	optype text,
-	ADD	overagency text,
-	ADD	overabbrev text,
-	ADD	overlevel text,
-	ADD	capacity text,
-	ADD	captype text,
-	ADD	proptype text;
+    ADD hash text, 
+    ADD facname text,
+    ADD factype text,
+    ADD facsubgrp text,
+    ADD facgroup text,
+    ADD facdomain text, 
+    ADD servarea text,
+    ADD opname text,
+    ADD opabbrev text,
+    ADD optype text,
+    ADD overagency text,
+    ADD overabbrev text,
+    ADD overlevel text,
+    ADD capacity text,
+    ADD captype text,
+    ADD proptype text;
 
 update qpl_libraries as t
-SET hash =  md5(CAST((t.*)AS text)), 
-	facname = name,
-	factype = 'Public Library',
-	facsubgrp = 'Public Libraries',	
-	facgroup = NULL,
-	facdomain = NULL,
-	servarea = NULL,
-	opname = NULL, 
-	opabbrev = NULL,
-	optype = 'Non-public',
-	overagency = NULL,
-	overabbrev = NULL, 
-	overlevel = NULL, 
-	capacity = NULL, 
-	captype = NULL, 
-	proptype = NULL
+SET hash =  md5(CAST((t.*)AS text)),
+	wkb_geometry = (CASE
+				        WHEN wkb_geometry is NULL 
+				            THEN ST_SetSRID(ST_Point(longitude, latitude), 4326)
+				        ELSE wkb_geometry
+				    END),
+    facname = name,
+    factype = 'Public Library',
+    facsubgrp = 'Public Libraries', 
+    facgroup = NULL,
+    facdomain = NULL,
+    servarea = NULL,
+    opname = 'Queens Public Library', 
+    opabbrev = NULL,
+    optype = 'Public',
+    overagency = 'Queens Public Library',
+    overabbrev = 'QPL', 
+    overlevel = NULL, 
+    capacity = NULL, 
+    captype = NULL, 
+    proptype = NULL
 ;
