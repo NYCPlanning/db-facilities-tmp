48fcd4ef47694a181752e518f857a6a0b5b28773
Baoling Zhou <baolingz@nyu.edu>
2019-05-22 12:39:20 -0400

    add usdot_airports, update usnps_parks

--- etl/usdot_airports.py
+++ etl/usdot_airports.py
@@ -1,49 +1,56 @@
 from dataflows import *
-from lib import dump_to_postgis, rename_field
+from lib import dump_to_postgis, rename_field, map_field
 import os
 import csv
 import sys
 from utils import url, fields, geo_flow, get_the_geom, quick_clean, get_hnum, get_sname
-from pathlib import Path
 
 csv.field_size_limit(sys.maxsize)
 
-table_name = 'dcla_culturalinstitutions'
-dcla_culturalinstitutions = Flow(
+table_name = 'usdot_airports'
+usdot_airports = Flow(
     load(url, resources = table_name, force_strings=False),
     checkpoint(table_name),
 
     add_field('datasource', 'string', table_name),
+
+    filter_rows(equals = [
+            dict(city = 'New York')
+            ]),
+
+    filter_rows(not_equals = [
+            dict(county = 'Suffolk')
+            ]),
 
     ################## geospatial ###################
     ###### Make sure the following columns ##########
     ###### exist before geo_flows          ########## 
     #################################################
 
-    add_computed_field([dict(target=dict(name = 'address_cleaned', type = 'string'),
-                                        operation=lambda row: quick_clean(row['address'])
-                                        ),
-                        dict(target=dict(name = 'hnum', type = 'string'),
-                                operation = lambda row: get_hnum(row['address_cleaned'])
-                                    ),
+
+    add_field('zipcode', 'string', ''),
+    add_field('hnum', 'string', ''),
+    rename_field('wkt', 'location'),
+
+    add_computed_field([
                             dict(target=dict(name = 'sname', type = 'string'),
-                                    operation=lambda row: get_sname(row['address_cleaned'])
+                                    operation=lambda row: quick_clean(row['fullname']) + ' Airport'
+                                    ),
+                            dict(target=dict(name = 'boro', type = 'string'),
+                                    operation=lambda row: row['county']
                                     )
                         ]),
-    delete_fields(fields=['address_cleaned']),
-
-    add_field('boro', 'string', ''),
-
-    rename_field('postcode', 'zipcode'),
 
     geo_flow,
     add_computed_field([dict(target=dict(name = 'the_geom', type = 'string'),
                             operation=lambda row: get_the_geom(row['geo_longitude'], row['geo_latitude'])
                             )
                         ]),
-    printer(num_rows=3),
+
+    delete_fields(fields=['boro']),
+
     dump_to_postgis()
 )
 
-dcla_culturalinstitutions.process()
+usdot_airports.process()
 
