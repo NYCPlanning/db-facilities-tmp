f86910a298b3650a6533a568b1ec36fd959daa74
sptkl <caobaiyue@gmail.com>
2019-05-30 15:27:13 -0400

    add other categories

--- sql/foodbankny_foodbanks.sql
+++ sql/foodbankny_foodbanks.sql
@@ -1,7 +1,6 @@
-
-select w.status::text, count(*) 
-from (select geo::json->'status' as status from foodbankny_foodbanks) w
-group by w.status::text;
+--select w.status::text, count(*) 
+--from (select geo::json->'status' as status from foodbankny_foodbanks) w
+--group by w.status::text;
 
 ALTER TABLE foodbankny_foodbanks
 	ADD hash text, 
@@ -22,13 +21,23 @@ ALTER TABLE foodbankny_foodbanks
 	ADD	proptype text;
 
 update foodbankny_foodbanks as t
-SET hash =  md5(CAST((t.*)AS text)), 
+SET hash =  md5(CAST((t.*)AS text)),
+	wkb_geometry = (CASE
+				        WHEN wkb_geometry is NULL 
+				        THEN ST_SetSRID(ST_Point(
+				        		lng::DOUBLE PRECISION, 
+				        		lat::DOUBLE PRECISION), 
+				        		4326)
+				        ELSE wkb_geometry
+					END),
 	facname = title,
 	factype = (CASE
 				WHEN categories = '64' THEN 'Food Pantry'
 				WHEN categories = '65' THEN 'Soup Kitchen'
+				WHEN categories = '63' THEN 'Senior Services'
+				WHEN categories = '66' THEN 'Financial Assistance and Social Services'
+				WHEN categories = '67' THEN 'Soup Kitchens and Food Pantries'
 			  END),
-		
 	facsubgrp = 'Soup Kitchens and Food Pantries',	
 	facgroup = NULL,
 	facdomain = NULL,
