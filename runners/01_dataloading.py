e5549d89e14a9e2450e8a4bc636aa0e1da24cc24
sptkl <caobaiyue@gmail.com>
2019-06-04 13:59:10 -0400

    updateing dohmh_daycare [skip ci]

--- runners/01_dataloading.py
+++ runners/01_dataloading.py
@@ -1,12 +1,12 @@
 import os
 
 recipes = ['dcp_pops', 'nypl_libraries', 'dohmh_daycare', 'doitt_libraries', 
-'qpl_libraries', 'nysdoh_healthfacilities', 'doe_busroutesgarages', 
+'qpl_libraries', 'dcas_colp', 'nysdoh_healthfacilities', 'doe_busroutesgarages', 
 'dca_operatingbusinesses', 'nysomh_mentalhealth', 'doe_lcgms', 'sbs_workforce1',
 'foodbankny_foodbanks', 'usdot_ports', 'dsny_mtsgaragemaintenance', 
-'dot_publicparking', 'nycha_policeservice', 'nysdec_solidwaste', 
+'dot_publicparking', 'dep_wwtc', 'nycha_policeservice', 'nysdec_solidwaste', 
 'usdot_airports', 'nysed_activeinstitutions', 'dcla_culturalinstitutions', 
-'dot_mannedfacilities', 'usnps_parks', 'bic_tradewaste', 'nysopwdd_providers', 
+'dot_mannedfacilities', 'usnps_parks', 'nysopwdd_providers', 
 'dfta_contracts', 'dpr_parksproperties', 'bpl_libraries']
 
 for i in recipes: 
