4e493766e2a144e5767355bebb430da3dbe48e87
bendnorman <bdn29@cornell.edu>
2019-06-07 12:24:10 -0400

    add nycha_communitycenters.py and sql

---
+++ sql/nycha_communitycenters.sql
@@ -0,0 +1,40 @@
+--select w.status::text, count(*) 
+--from (select geo::json->'status' as status from bpl_libraries) w
+--group by w.status::text;
+
+ALTER TABLE nycha_communitycenters
+	ADD hash text, 
+	ADD	facname text,
+	ADD	factype text,
+	ADD	facsubgrp text,
+	ADD	facgroup text,
+	ADD	facdomain text, 
+	ADD	servarea text,
+	ADD	opname text,
+	ADD	opabbrev text,
+	ADD	optype text,
+	ADD	overagency text,
+	ADD	overabbrev text,
+	ADD	overlevel text,
+	ADD	capacity text,
+	ADD	captype text,
+	ADD	proptype text;
+
+update nycha_communitycenters as t
+SET hash =  md5(CAST((t.*)AS text)), 
+	facname = development,
+	factype = program_type,
+	facsubgrp = NULL,
+	facgroup = 'Human Services',
+	facdomain = 'Health and Human Services',
+	servarea = NULL,
+	opname = 'New York City Housing Authority',
+	opabbrev = 'NYCHA',
+	optype = 'Public',
+	overagency = 'New York City Housing Authority',
+	overabbrev = 'NYCHA', 
+	overlevel = NULL, 
+	capacity = NULL, 
+	captype = NULL, 
+	proptype = NULL
+;
