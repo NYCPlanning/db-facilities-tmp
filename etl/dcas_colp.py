ae76a1b1492e5424c39bdef9c3f81bd7c5b6ebfa
sptkl <caobaiyue@gmail.com>
2019-06-13 12:17:57 -0400

    adding facilities_classification

--- etl/dcas_colp.py
+++ etl/dcas_colp.py
@@ -5,7 +5,7 @@ import csv
 import sys
 from pathlib import Path
 import re
-from utils import url, geo_flow, get_the_geom
+from utils import url, geo_flow, get_the_geom, quick_clean, get_hnum, get_sname
 
 csv.field_size_limit(sys.maxsize)
 
@@ -13,26 +13,31 @@ table_name = 'dcas_colp'
 dcas_colp = Flow(
     load(url, resources = table_name, force_strings=True),
 
-    checkpoint(table_name),
-
     add_field('datasource', 'string', table_name),
 
     ################## geospatial ###################
     ###### Make sure the following columns ##########
     ###### exist before geo_flows          ########## 
     #################################################
-
+    
     add_field('zipcode', 'string', ''),
-    rename_field('house_number', 'hnum'),
-    rename_field('street_name', 'sname'), 
-    rename_field('borough', 'boro'), 
+   
+    rename_field('borough', 'boro'),
 
+    rename_field('wkt', 'point_location'),
+
+    add_computed_field([dict(target=dict(name = 'hnum', type = 'string'),
+                                operation = lambda row: get_hnum(row['address'])
+                                ),
+                        dict(target=dict(name = 'sname', type = 'string'),
+                                    operation=lambda row: get_sname(row['address'])
+                                )
+                        ]),
     geo_flow,
     add_computed_field([dict(target=dict(name = 'the_geom', type = 'string'),
                             operation=lambda row: get_the_geom(row['geo_longitude'], row['geo_latitude'])
                             )
                         ]),
-                        
     dump_to_postgis(),
 )
 
---
+++ etl/facilities_classification.py
@@ -0,0 +1,19 @@
+from dataflows import *
+from lib import dump_to_postgis, rename_field
+import os
+import csv
+import sys
+from pathlib import Path
+import re
+from utils import url
+
+csv.field_size_limit(sys.maxsize)
+
+table_name = 'facilities_classification'
+facilities_classification = Flow(
+    load(url, resources = table_name, force_strings=True),
+    dump_to_postgis()
+)
+
+facilities_classification.process()
+
