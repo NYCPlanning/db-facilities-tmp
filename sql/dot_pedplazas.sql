bb1cc509b5afde51e5fe18c95717c019d3e20482
Baoling Zhou <baolingz@nyu.edu>
2019-06-14 15:55:15 -0400

    add dot_pedplazas.py and sql

---
+++ sql/dot_pedplazas.sql
@@ -0,0 +1,45 @@
+--select w.status::text, count(*) 
+--from (select geo::json->'status' as status from dot_pedplazas) w
+--group by w.status::text;
+
+ALTER TABLE dot_pedplazas
+	ADD hash text, 
+	ADD	facname text,
+	ADD	factype text,
+	ADD	facsubgrp text,
+	ADD	facgroup text,
+	ADD	facdomain text, 
+	ADD	servarea text,
+	ADD	opname text,
+	ADD	opabbrev text,
+	ADD	optype text,
+	ADD	overagency text,
+	ADD	overabbrev text,
+	ADD	overlevel text,
+	ADD	capacity text,
+	ADD	captype text,
+	ADD	proptype text;
+
+update dot_pedplazas as t
+SET hash =  md5(CAST((t.*)AS text)), 
+    wkb_geometry = (CASE
+						WHEN wkb_geometry is NULL 
+						THEN ST_GeometryFromText(point_location, 4326)
+						ELSE wkb_geometry
+					END),
+	facname = plaza_nam,
+	factype = 'Pedestrian Plaza',
+	facsubgrp = 'Streetscapes, Plazas, and Malls',
+	facgroup = NULL,
+	facdomain = NULL,
+	servarea = NULL,
+	opname = 'NYC Department of Transportation',
+	opabbrev = 'NYCDOT',
+	optype = 'Public',
+	overagency = 'NYC Department of Transportation',
+	overabbrev = 'NYCDOT', 
+	overlevel = NULL, 
+	capacity = NULL, 
+	captype = NULL, 
+	proptype = NULL
+;
