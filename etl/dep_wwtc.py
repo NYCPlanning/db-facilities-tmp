c93820f32ae755ce53d19b3afc1b5046ee73639d
sptkl <caobaiyue@gmail.com>
2019-06-17 11:48:56 -0400

    adding dep_wwtp, needs further implementation on optype

--- etl/dep_wwtc.py
+++ etl/dep_wwtc.py
@@ -12,41 +12,28 @@ csv.field_size_limit(sys.maxsize)
 table_name = 'dep_wwtc'
 dep_wwtc = Flow(
     load(url, resources = table_name, force_strings=True),
-    # cacheing table
-    checkpoint(table_name),
     # datasource
     add_field('datasource', 'string', table_name),
-
-
+    
     ################## geospatial ###################
     ###### Make sure the following columns ##########
     ###### exist before geo_flows          ########## 
     #################################################
 
     # hnum
+    rename_field('house_number', 'hnum'),
     # sname 
-    # add_computed_field([dict(target=dict(name = 'address', type = 'string'),
-    #                                     operation=lambda row: quick_clean(row['program_address'])
-    #                                     ),
-    #                     dict(target=dict(name = 'hnum', type = 'string'),
-    #                             operation = lambda row: get_hnum(row['address'])
-    #                                 ),
-    #                         dict(target=dict(name = 'sname', type = 'string'),
-    #                                 operation=lambda row: get_sname(row['address'])
-    #                                 )
-    #                     ]),
-    # # boro
-    # add_field('boro', 'string', ''),
-    # # zipcode
-    # rename_field('program_zipcode', 'zipcode'),
+    rename_field('street_name', 'sname'),
+    # boro
+    add_field('boro', 'string', ''),
+    # zipcode --> zipcode
 
-    # geo_flow,
-    # add_computed_field([dict(target=dict(name = 'the_geom', type = 'string'),
-    #                         operation=lambda row: get_the_geom(row['geo_longitude'], row['geo_latitude'])
-    #                         )
-    #                     ]),
-    printer(num_rows=3),
-    # dump_to_postgis()
+    geo_flow,
+    add_computed_field([dict(target=dict(name = 'the_geom', type = 'string'),
+                            operation=lambda row: get_the_geom(row['geo_longitude'], row['geo_latitude'])
+                            )
+                        ]),
+    dump_to_postgis()
 )
 
 dep_wwtc.process()
