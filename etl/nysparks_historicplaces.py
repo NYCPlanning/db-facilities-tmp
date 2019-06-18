b40c3678ad1e0e7a10b208b8047793cedc44e65a
bendnorman <bdn29@cornell.edu>
2019-06-18 13:41:30 -0400

    Added nysparks_historicplaces.py and .sql

---
+++ etl/nysparks_historicplaces.py
@@ -0,0 +1,30 @@
+from dataflows import *
+from lib import dump_to_postgis, rename_field, map_field
+import os
+import csv
+import sys
+from pathlib import Path
+from utils import url, geo_flow, get_the_geom, quick_clean, get_hnum, get_sname, convert_to_boro
+
+csv.field_size_limit(sys.maxsize)
+
+table_name = 'nysparks_historicplaces'
+
+nysparks_historicplaces = Flow(
+    load(url, resources=table_name, force_strings=True),
+    add_field('datasource', 'string', table_name),
+    
+    filter_rows(equals=[dict(county='New York'),
+                        dict(county='Kings'),
+                        dict(county='Queens'),
+                        dict(county='Bronx'),
+                        dict(county='Richmond')]),
+    add_computed_field([
+        dict(target=dict(name='boro', type='string'),
+             operation=lambda row: convert_to_boro(row['county'])
+             )
+    ]),
+
+    dump_to_postgis()
+)
+nysparks_historicplaces.process()
---
+++ sql/nysparks_historicplaces.sql
@@ -0,0 +1,50 @@
+--select w.status::text, count(*) 
+--from (select geo::json->'status' as status from doe_busroutesgarages) w
+--group by w.status::text;
+
+ALTER TABLE nysparks_historicplaces
+	ADD hash text,
+    ADD wkb_geometry geometry, 
+	ADD	facname text,
+	ADD	factype text,
+	ADD	facsubgrp text,
+	ADD	facgroup text,
+	ADD	facdomain text, 
+	ADD	servarea text,
+	ADD	opname text,
+	ADD	opabbrev text,
+	ADD	optype text,
+	ADD	overagency text,
+	ADD	overabbrev text,
+	ADD	overlevel text,
+	ADD	capacity text,
+	ADD	captype text,
+	ADD	proptype text;
+
+update nysparks_historicplaces as t
+SET hash =  md5(CAST((t.*)AS text)), 
+    wkb_geometry = ST_SetSRID(ST_Point(
+            longitude::DOUBLE PRECISION, 
+            latitude::DOUBLE PRECISION), 
+            4326),
+	facname = resource_name,
+	factype = 'State Historic Place',
+	facsubgrp = 'Historical Sites',
+	facgroup = NULL,
+	facdomain = NULL,
+	servarea = NULL,
+	opname = NULL,
+	opabbrev = NULL,
+	optype = 'Non-public',
+	overagency = 'The New York State Office of Parks, Recreation and Historic Preservation',
+	overabbrev = 'NYSOPRHP',
+	overlevel = 'State',
+	capacity = NULL,
+	captype = NULL,
+	proptype = NULL
+;
+
+
+
+
+
