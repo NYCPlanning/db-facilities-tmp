6f7701d5f92539fea2615c88cf32c93a67cd0b65
bendnorman <bdn29@cornell.edu>
2019-06-18 15:40:55 -0400

    Added nysdoccs_corrections.py and .sql

---
+++ sql/nysdoccs_corrections.sql
@@ -0,0 +1,41 @@
+--select w.status::text, count(*) 
+--from (select geo::json->'status' as status from doe_busroutesgarages) w
+--group by w.status::text;
+
+ALTER TABLE nysdoccs_corrections
+	ADD hash text, 
+	ADD facname text,
+	ADD factype text,
+	ADD facsubgrp text,
+	ADD facgroup text,
+	ADD facdomain text,
+	ADD servarea text,
+	ADD opname text,
+	ADD opabbrev text,
+	ADD optype text,
+	ADD overagency text,
+	ADD overabbrev text,
+	ADD overlevel text,
+	ADD capacity text,
+	ADD captype text,
+	ADD proptype text;
+
+update nysdoccs_corrections as t
+SET hash =  md5(CAST((t.*)AS text)), 
+	facname = facility_name,
+	factype = 'Correctional Facility',
+	facsubgrp = 'Correctional Facility',
+	facgroup = NULL,
+	facdomain = NULL,
+	servarea = NULL,
+	opname = 'NYS Department of Corrections and Community Supervision',
+	opabbrev = 'NYSDOCCS',
+	optype = 'Public',
+	overagency = NULL,
+	overabbrev = NULL,
+	overlevel = 'State',
+	capacity = NULL,
+	captype = NULL,
+	proptype = NULL
+;
+
