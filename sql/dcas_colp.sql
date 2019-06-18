91737564b7397b963418593736b7fec3a27f7381
sptkl <caobaiyue@gmail.com>
2019-06-18 13:59:13 -0400

    update colp sql

--- sql/dcas_colp.sql
+++ sql/dcas_colp.sql
@@ -1,9 +1,10 @@
 --select w.status::text, count(*) 
 --from (select geo::json->'status' as status from dcas_colp) w
 --group by w.status::text;
+
 CREATE TABLE dcas_colp_tmp as
 SELECT * FROM dcas_colp
-WHERE usedec ~* 'maintenance|storage|Infrastructure|Office|residential|no use';
+WHERE colp_type ~* 'maintenance|storage|Infrastructure|Office|residential|no use|private';
 
 DROP TABLE dcas_colp;
 
@@ -31,8 +32,8 @@ ALTER TABLE dcas_colp
 UPDATE dcas_colp as t
 SET hash =  md5(CAST((t.*)AS text)), 
 	facname = (CASE
-				WHEN (name = ' ' OR name IS NULL) AND usedec LIKE '%OFFICE%' THEN 'Office'
-				WHEN (name = ' ' OR name IS NULL) AND usedec LIKE '%NO USE%' THEN 'City Owned Property'
+				WHEN (name = ' ' OR name IS NULL) AND usedec ~* 'office' THEN 'Office'
+				WHEN (name = ' ' OR name IS NULL) AND usedec ~* 'no use' THEN 'City Owned Property'
 				WHEN name <> ' ' AND name IS NOT NULL THEN initcap(name)
 				ELSE initcap(REPLACE(usedec, 'OTHER ', ''))
 			END),
@@ -335,18 +336,543 @@ SET hash =  md5(CAST((t.*)AS text)),
 	facgroup = NULL,
 	facdomain = NULL,
 	servarea = NULL,
-	opname = NULL,
-	opabbrev = NULL,
-	optype = NULL,
-	overagency = NULL,
-	overabbrev = NULL, 
+	opname = (CASE
+
+				WHEN agency='ACS' THEN 'NYC Administration for Childrens Services'
+	
+				WHEN agency='ACTRY' THEN 'NYC Office of the Actuary'
+	
+				WHEN agency='AGING' THEN 'NYC Department for the Aging'
+	
+				WHEN agency='BIC' THEN 'NYC Business Integrity Commission'
+	
+				WHEN agency='BLDGS' THEN 'NYC Department of Buildings'
+	
+				WHEN agency='BP-BK' THEN 'NYC Borough President - Brooklyn'
+	
+				WHEN agency='BP-BX' THEN 'NYC Borough President - Bronx'
+	
+				WHEN agency='BP-MN' THEN 'NYC Borough President - Manhattan'
+	
+				WHEN agency='BP-QN' THEN 'NYC Borough President - Queens'
+	
+				WHEN agency='BP-SI' THEN 'NYC Borough President - Staten Island'
+	
+				WHEN agency='BPL' THEN 'Brooklyn Public Library'
+	
+				WHEN agency='BSA' THEN 'NYC Board of Standards and Appeals'
+	
+				WHEN agency LIKE 'CB%' THEN REPLACE(CONCAT('NYC ', initcap(boro), ' Community Board ', RIGHT(agency,2)),' 0',' ')
+	
+				WHEN agency='CCRB' THEN 'NYC Civilian Complaint Review Board'
+	
+				WHEN agency='CEO' THEN 'NYC Center for Economic Opportunity'
+	
+				WHEN agency='CFB' THEN 'NYC Campaign Finance Board'
+	
+				WHEN agency='CIVIL' THEN 'NYC Civil Service Commission'
+	
+				WHEN agency='CLERK' THEN 'NYC Office of the City Clerk'
+	
+				WHEN agency='COIB' THEN 'NYC Conflict of Interest Board'
+	
+				WHEN agency='COMPT' THEN 'NYC Office of the Comptroller'
+	
+				WHEN agency='CORR' THEN 'NYC Department of Correction'
+	
+				WHEN agency='COUNC' THEN 'NYC City Council'
+	
+				WHEN agency='COURT' THEN 'NYS Unified Court System'
+	
+				WHEN agency='CULT' THEN 'NYC Department of Cultural Affairs'
+	
+				WHEN agency='CUNY' THEN 'City University of New York'
+	
+				WHEN agency='DA-BK' THEN 'NYC District Attorney - Brooklyn'
+	
+				WHEN agency='DA-BX' THEN 'NYC District Attorney - Bronx '
+	
+				WHEN agency='DA-MN' THEN 'NYC District Attorney - Manhattan'
+	
+				WHEN agency='DA-QN' THEN 'NYC District Attorney - Queens'
+	
+				WHEN agency='DA-SI' THEN 'NYC District Attorney - Staten Island'
+	
+				WHEN agency='DA-SP' THEN 'NYC District Attorney - Office Special Narcotics'
+	
+				WHEN agency='DCA' THEN 'NYC Department of Consumer Affairs'
+	
+				WHEN agency='DCAS' THEN 'NYC Department of Citywide Administrative Services'
+	
+				WHEN agency='DDC' THEN 'NYC Department of Design and Construction'
+	
+				WHEN agency='DEP' THEN 'NYC Department of Environmental Protection'
+	
+				WHEN agency='DHS' THEN 'NYC Department of Homeless Services'
+	
+				WHEN agency='DOI' THEN 'NYC Department of Investigation'
+	
+				WHEN agency='DOITT' THEN 'NYC Department of Information Technology and Telecommunications'
+	
+				WHEN agency='DORIS' THEN 'NYC Department of Records and Information Services'
+	
+				WHEN agency='DOT' THEN 'NYC Department of Transportation'
+	
+				WHEN agency='DSBS' THEN 'NYC Department of Small Business Services'
+	
+				WHEN agency='DYCD' THEN 'NYC Department of Youth and Community Development'
+	
+				WHEN agency='EDC' THEN 'NYC Economic Development Corporation'
+	
+				WHEN agency='EDUC' THEN 'NYC Department of Education'
+	
+				WHEN agency='ELECT' THEN 'NYC Board of Elections'
+	
+				WHEN agency='FINAN' THEN 'NYC Department of Finance'
+	
+				WHEN agency='FIRE' THEN 'NYC Fire Department'
+	
+				WHEN agency='FISA' THEN 'NYC Financial Information Services Agency'
+	
+				WHEN agency='HHC' THEN 'NYC Health and Hospitals Corporation'
+	
+				WHEN agency='HLTH' THEN 'NYC Department of Health and Mental Hygiene'
+	
+				WHEN agency='HPD' THEN 'NYC Department of Housing Preservation and Development'
+	
+				WHEN agency='HRA' THEN 'NYC Human Resources Administration/Department of Social Services'
+	
+				WHEN agency='HUMRT' THEN 'NYC Commission on Human Rights'
+	
+				WHEN agency='HYDC' THEN 'Hudson Yards Development Corporation'
+	
+				WHEN agency='IBO' THEN 'NYC Independent Budget Office'
+	
+				WHEN agency='LAW' THEN 'NYC Law Department'
+	
+				WHEN agency='LDMKS' THEN 'NYC Landmarks Preservation Commission'
+	
+				WHEN agency='MAYOR' THEN 'NYC Office of the Mayor'
+	
+				WHEN agency='MOME' THEN 'NYC Office of the Mayor'
+	
+				WHEN agency='MTA' THEN 'Metropolitan Transportation Authority'
+	
+				WHEN agency='NYCHA' THEN 'NYC Housing Authority'
+	
+				WHEN agency='NYCTA' THEN 'Metropolitan Transportation Authority'
+	
+				WHEN agency='NYPD' THEN 'NYC Police Department'
+	
+				WHEN agency='NYPL' THEN 'New York Public Library'
+	
+				WHEN agency='OATH' THEN 'NYC Office of Administrative Trials and Hearings'
+	
+				WHEN agency='OCA' THEN 'NYC Office of Court Administration'
+	
+				WHEN agency='OCB' THEN 'NYC Office of Collective Bargaining'
+	
+				WHEN agency='OCME' THEN 'NYC Office of the Medical Examiner'
+	
+				WHEN agency='OEM' THEN 'NYC Office of Emergency Management'
+	
+				WHEN agency='OLR' THEN 'NYC Office of Labor Relations'
+	
+				WHEN agency='OMB' THEN 'NYC Office of Management and Budget'
+	
+				WHEN agency='OPA' THEN 'NYC Office of Payroll Administration'
+	
+				WHEN agency='PA-BK' THEN 'NYC Public Administrators Office - Brooklyn'
+	
+				WHEN agency='PA-BX' THEN 'NYC Public Administrators Office - Bronx'
+	
+				WHEN agency='PA-MN' THEN 'NYC Public Administrators Office - Manhattan'
+	
+				WHEN agency='PA-QN' THEN 'NYC Public Administrators Office - Queens'
+	
+				WHEN agency='PA-SI' THEN 'NYC Public Administrators Office - Staten Island'
+	
+				WHEN agency='PARKS' THEN 'NYC Department of Parks and Recreation'
+	
+				WHEN agency='PBADV' THEN 'NYC Office of Public Advocate'
+	
+				WHEN agency='PLAN' THEN 'NYC Department of City Planning'
+	
+				WHEN agency='PRIV' THEN 'Non-public'
+	
+				WHEN agency='PROB' THEN 'NYC Department of Probation'
+	
+				WHEN agency='QPL' THEN 'Queens Public Library'
+	
+				WHEN agency='SANIT' THEN 'NYC Department of Sanitation'
+	
+				WHEN agency='TAXCM' THEN 'NYC Tax Commission'
+	
+				WHEN agency='TBTA' THEN 'Metropolitan Transportation Authority'
+	
+				WHEN agency='TLC' THEN 'NYC Taxi and Limousine Commission'
+	
+				WHEN agency='UNKN' THEN 'NYC Unknown'
+	
+			END),
+	opabbrev = (CASE
+	
+				WHEN agency='HYDC' THEN 'HYDC'
+	
+				WHEN agency='MTA' THEN 'MTA'
+	
+				WHEN agency='NYCTA' THEN 'MTA'
+	
+				WHEN agency='TBTA' THEN 'MTA'
+	
+				WHEN agency='PARKS' THEN 'NYCDPR'
+	
+				WHEN agency='BLDGS' THEN 'NYCDOB'
+	
+				WHEN agency='BPL' THEN 'BPL'
+	
+				WHEN agency='NYPL' THEN 'NYPL'
+	
+				WHEN agency='QPL' THEN 'QPL'
+	
+				WHEN agency='SANIT' THEN 'NYCDSNY'
+	
+				WHEN agency='AGING' THEN 'NYCDFTA'
+	
+				WHEN agency='EDUC' THEN 'NYCDOE'
+	
+				WHEN agency='CULT' THEN 'NYCDCLA'
+	
+				WHEN agency='CORR' THEN 'NYCDOC'
+	
+				WHEN agency='FIRE' THEN 'NYCFDNY'
+	
+				WHEN agency='HLTH' THEN 'NYCDOHMH'
+	
+				WHEN agency='ELECT' THEN 'NYCBOE'
+	
+				WHEN agency='CIVIL' THEN 'NYCCSC'
+	
+				WHEN agency='HUMRT' THEN 'NYCCCHR'
+	
+				WHEN agency='COUNC' THEN 'NYCCOUNCIL'
+	
+				WHEN agency='PLAN' THEN 'NYCDCP'
+	
+				WHEN agency='FINAN' THEN 'NYCDOF'
+	
+				WHEN agency='PROB' THEN 'NYCDOP'
+	
+				WHEN agency='DSBS' THEN 'NYCSBS'
+	
+				WHEN agency='FIRE' THEN 'FDNY'
+	
+				WHEN agency='NYPD' THEN 'NYPD'
+	
+				WHEN agency='HRA' THEN 'NYCHRA/DSS'
+	
+				WHEN agency='DA-SP' THEN 'NYCDA-SNP'
+	
+				WHEN agency='LDMKS' THEN 'NYCLPC'
+	
+				WHEN agency='OEM' THEN 'NYCEM'
+	
+				WHEN agency='PBADV' THEN 'NYCPA'
+	
+				WHEN agency='ACTRY' THEN 'NYCACT'
+	
+				WHEN agency='COMPT' THEN 'NYCCOMP'
+	
+				WHEN agency='MAYOR' THEN 'NYCMO'
+	
+				WHEN agency='MOME' THEN 'NYCMO'
+	
+				WHEN agency='TAX' THEN 'NYCTC'
+	
+				WHEN agency='COURT' THEN 'NYCOURTS'
+	
+				WHEN agency='CUNY' THEN 'CUNY'
+	
+				WHEN agency='PRIV' THEN 'Non-public'
+	
+				WHEN agency='UNKN' THEN 'NYC-Unknown'
+	
+				ELSE CONCAT('NYC',agency)
+	
+			END),
+	optype = (CASE
+				WHEN agency = 'PRIV' THEN 'Non-public'
+				ELSE 'Public'
+			END),
+	overagency = (CASE
+	
+				WHEN agency='ACS' THEN 'NYC Administration for Childrens Services'
+	
+				WHEN agency='ACTRY' THEN 'NYC Office of the Actuary'
+	
+				WHEN agency='AGING' THEN 'NYC Department for the Aging'
+	
+				WHEN agency='BIC' THEN 'NYC Business Integrity Commission'
+	
+				WHEN agency='BLDGS' THEN 'NYC Department of Buildings'
+	
+				WHEN agency='BP-BK' THEN 'NYC Borough President - Brooklyn'
+	
+				WHEN agency='BP-BX' THEN 'NYC Borough President - Bronx'
+	
+				WHEN agency='BP-MN' THEN 'NYC Borough President - Manhattan'
+	
+				WHEN agency='BP-QN' THEN 'NYC Borough President - Queens'
+	
+				WHEN agency='BP-SI' THEN 'NYC Borough President - Staten Island'
+	
+				WHEN agency='BPL' THEN 'Brooklyn Public Library'
+	
+				WHEN agency='BSA' THEN 'NYC Board of Standards and Appeals'
+	
+				WHEN agency LIKE 'CB%' THEN REPLACE(CONCAT('NYC ', initcap(boro), ' Community Board ', RIGHT(agency,2)),' 0',' ')
+	
+				WHEN agency='CCRB' THEN 'NYC Civilian Complaint Review Board'
+	
+				WHEN agency='CEO' THEN 'NYC Center for Economic Opportunity'
+	
+				WHEN agency='CFB' THEN 'NYC Campaign Finance Board'
+	
+				WHEN agency='CIVIL' THEN 'NYC Civil Service Commission'
+	
+				WHEN agency='CLERK' THEN 'NYC Office of the City Clerk'
+	
+				WHEN agency='COIB' THEN 'NYC Conflict of Interest Board'
+	
+				WHEN agency='COMPT' THEN 'NYC Office of the Comptroller'
+	
+				WHEN agency='CORR' THEN 'NYC Department of Correction'
+	
+				WHEN agency='COUNC' THEN 'NYC City Council'
+	
+				WHEN agency='COURT' THEN 'NYS Unified Court System'
+	
+				WHEN agency='CULT' THEN 'NYC Department of Cultural Affairs'
+	
+				WHEN agency='CUNY' THEN 'City University of New York'
+	
+				WHEN agency='DA-BK' THEN 'NYC District Attorney - Brooklyn'
+	
+				WHEN agency='DA-BX' THEN 'NYC District Attorney - Bronx '
+	
+				WHEN agency='DA-MN' THEN 'NYC District Attorney - Manhattan'
+	
+				WHEN agency='DA-QN' THEN 'NYC District Attorney - Queens'
+	
+				WHEN agency='DA-SI' THEN 'NYC District Attorney - Staten Island'
+	
+				WHEN agency='DA-SP' THEN 'NYC District Attorney - Office Special Narcotics'
+	
+				WHEN agency='DCA' THEN 'NYC Department of Consumer Affairs'
+	
+				WHEN agency='DCAS' THEN 'NYC Department of Citywide Administrative Services'
+	
+				WHEN agency='DDC' THEN 'NYC Department of Design and Construction'
+	
+				WHEN agency='DEP' THEN 'NYC Department of Environmental Protection'
+	
+				WHEN agency='DHS' THEN 'NYC Department of Homeless Services'
+	
+				WHEN agency='DOI' THEN 'NYC Department of Investigation'
+	
+				WHEN agency='DOITT' THEN 'NYC Department of Information Technology and Telecommunications'
+	
+				WHEN agency='DORIS' THEN 'NYC Department of Records and Information Services'
+	
+				WHEN agency='DOT' THEN 'NYC Department of Transportation'
+	
+				WHEN agency='DSBS' THEN 'NYC Department of Small Business Services'
+	
+				WHEN agency='DYCD' THEN 'NYC Department of Youth and Community Development'
+	
+				WHEN agency='EDC' THEN 'NYC Economic Development Corporation'
+	
+				WHEN agency='EDUC' THEN 'NYC Department of Education'
+	
+				WHEN agency='ELECT' THEN 'NYC Board of Elections'
+	
+				WHEN agency='FINAN' THEN 'NYC Department of Finance'
+	
+				WHEN agency='FIRE' THEN 'NYC Fire Department'
+	
+				WHEN agency='FISA' THEN 'NYC Financial Information Services Agency'
+	
+				WHEN agency='HHC' THEN 'NYC Health and Hospitals Corporation'
+	
+				WHEN agency='HLTH' THEN 'NYC Department of Health and Mental Hygiene'
+	
+				WHEN agency='HPD' THEN 'NYC Department of Housing Preservation and Development'
+	
+				WHEN agency='HRA' THEN 'NYC Human Resources Administration/Department of Social Services'
+	
+				WHEN agency='HUMRT' THEN 'NYC Commission on Human Rights'
+	
+				WHEN agency='HYDC' THEN 'Hudson Yards Development Corporation'
+	
+				WHEN agency='IBO' THEN 'NYC Independent Budget Office'
+	
+				WHEN agency='LAW' THEN 'NYC Law Department'
+	
+				WHEN agency='LDMKS' THEN 'NYC Landmarks Preservation Commission'
+	
+				WHEN agency='MAYOR' THEN 'NYC Office of the Mayor'
+	
+				WHEN agency='MOME' THEN 'NYC Office of the Mayor'
+	
+				WHEN agency='MTA' THEN 'Metropolitan Transportation Authority'
+	
+				WHEN agency='NYCHA' THEN 'NYC Housing Authority'
+	
+				WHEN agency='NYCTA' THEN 'Metropolitan Transportation Authority'
+	
+				WHEN agency='NYPD' THEN 'NYC Police Department'
+	
+				WHEN agency='NYPL' THEN 'New York Public Library'
+	
+				WHEN agency='OATH' THEN 'NYC Office of Administrative Trials and Hearings'
+	
+				WHEN agency='OCA' THEN 'NYC Office of Court Administration'
+	
+				WHEN agency='OCB' THEN 'NYC Office of Collective Bargaining'
+	
+				WHEN agency='OCME' THEN 'NYC Office of the Medical Examiner'
+	
+				WHEN agency='OEM' THEN 'NYC Office of Emergency Management'
+	
+				WHEN agency='OLR' THEN 'NYC Office of Labor Relations'
+	
+				WHEN agency='OMB' THEN 'NYC Office of Management and Budget'
+	
+				WHEN agency='OPA' THEN 'NYC Office of Payroll Administration'
+	
+				WHEN agency='PA-BK' THEN 'NYC Public Administrators Office - Brooklyn'
+	
+				WHEN agency='PA-BX' THEN 'NYC Public Administrators Office - Bronx'
+	
+				WHEN agency='PA-MN' THEN 'NYC Public Administrators Office - Manhattan'
+	
+				WHEN agency='PA-QN' THEN 'NYC Public Administrators Office - Queens'
+	
+				WHEN agency='PA-SI' THEN 'NYC Public Administrators Office - Staten Island'
+	
+				WHEN agency='PARKS' THEN 'NYC Department of Parks and Recreation'
+	
+				WHEN agency='PBADV' THEN 'NYC Office of Public Advocate'
+	
+				WHEN agency='PLAN' THEN 'NYC Department of City Planning'
+	
+				WHEN agency='PRIV' THEN 'Non-public'
+	
+				WHEN agency='PROB' THEN 'NYC Department of Probation'
+	
+				WHEN agency='QPL' THEN 'Queens Public Library'
+	
+				WHEN agency='SANIT' THEN 'NYC Department of Sanitation'
+	
+				WHEN agency='TAXCM' THEN 'NYC Tax Commission'
+	
+				WHEN agency='TBTA' THEN 'Metropolitan Transportation Authority'
+	
+				WHEN agency='TLC' THEN 'NYC Taxi and Limousine Commission'
+	
+				WHEN agency='UNKN' THEN 'NYC Unknown'
+	
+			END),
+	overabbrev = (CASE
+	
+				WHEN agency='HYDC' THEN 'HYDC'
+	
+				WHEN agency='MTA' THEN 'MTA'
+	
+				WHEN agency='NYCTA' THEN 'MTA'
+	
+				WHEN agency='TBTA' THEN 'MTA'
+	
+				WHEN agency='PARKS' THEN 'NYCDPR'
+	
+				WHEN agency='BLDGS' THEN 'NYCDOB'
+	
+				WHEN agency='BPL' THEN 'BPL'
+	
+				WHEN agency='NYPL' THEN 'NYPL'
+	
+				WHEN agency='QPL' THEN 'QPL'
+	
+				WHEN agency='SANIT' THEN 'NYCDSNY'
+	
+				WHEN agency='AGING' THEN 'NYCDFTA'
+	
+				WHEN agency='EDUC' THEN 'NYCDOE'
+	
+				WHEN agency='CULT' THEN 'NYCDCLA'
+	
+				WHEN agency='CORR' THEN 'NYCDOC'
+	
+				WHEN agency='FIRE' THEN 'NYCFDNY'
+	
+				WHEN agency='HLTH' THEN 'NYCDOHMH'
+	
+				WHEN agency='ELECT' THEN 'NYCBOE'
+	
+				WHEN agency='CIVIL' THEN 'NYCCSC'
+	
+				WHEN agency='HUMRT' THEN 'NYCCCHR'
+	
+				WHEN agency='COUNC' THEN 'NYCCOUNCIL'
+	
+				WHEN agency='PLAN' THEN 'NYCDCP'
+	
+				WHEN agency='FINAN' THEN 'NYCDOF'
+	
+				WHEN agency='PROB' THEN 'NYCDOP'
+	
+				WHEN agency='DSBS' THEN 'NYCSBS'
+	
+				WHEN agency='FIRE' THEN 'FDNY'
+	
+				WHEN agency='NYPD' THEN 'NYPD'
+	
+				WHEN agency='HRA' THEN 'NYCHRA/DSS'
+	
+				WHEN agency='DA-SP' THEN 'NYCDA-SNP'
+	
+				WHEN agency='LDMKS' THEN 'NYCLPC'
+	
+				WHEN agency='OEM' THEN 'NYCEM'
+	
+				WHEN agency='PBADV' THEN 'NYCPA'
+	
+				WHEN agency='ACTRY' THEN 'NYCACT'
+	
+				WHEN agency='COMPT' THEN 'NYCCOMP'
+	
+				WHEN agency='MAYOR' THEN 'NYCMO'
+	
+				WHEN agency='MOME' THEN 'NYCMO'
+	
+				WHEN agency='TAX' THEN 'NYCTC'
+	
+				WHEN agency='COURT' THEN 'NYCOURTS'
+	
+				WHEN agency='CUNY' THEN 'CUNY'
+	
+				WHEN agency='PRIV' THEN 'Non-public'
+	
+				WHEN agency='UNKN' THEN 'NYC-Unknown'
+	
+				ELSE CONCAT('NYC',agency)
+	
+			END), 
 	overlevel = NULL, 
 	capacity = NULL, 
 	captype = NULL, 
-	proptype = NULL
+	proptype = (CASE
+				WHEN type='OF' THEN 'City Owned'
+				WHEN type='LF' THEN 'City Leased'
+			END)
 ;
-
-
-
-select distinct(usedec) from dcas_colp
-where usedec ~* 'maintenance|storage|Infrastructure|Office|residential|no use';
