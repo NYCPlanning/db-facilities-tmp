3132161fc8117c30c1b04e505eb94e870619b163
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-06-18 15:35:20 -0400

    adding runner to export outputs

---
+++ runners/03_export.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+start=$(date +'%T')
+
+echo "export FacDB outputs"
+
+# Facilities data table
+psql $DATAFLOWS_DB_ENGINE -c "\copy (SELECT * FROM facilities) 
+                                TO '/home/db-facilities/output/facilities.csv' 
+                                DELIMITER ',' CSV HEADER;"
+# Facilities shapefile
+pgsql2shp -U $DBUSER -p $PORT -h HOST -f \
+    /home/db-facilities/output/facilities $DBNAME \
+    'SELECT * 
+    FROM facilities
+    WHERE geom IS NOT NULL;'
+
+# QC reports
+psql $DATAFLOWS_DB_ENGINE -c "\copy (SELECT * FROM qc_operator) 
+                                TO '/home/db-facilities/output/qc_operator.csv' 
+                                DELIMITER ',' CSV HEADER;"
+
+psql $DATAFLOWS_DB_ENGINE -c "\copy (SELECT * FROM qc_oversight) 
+                                TO '/home/db-facilities/output/qc_oversight.csv' 
+                                DELIMITER ',' CSV HEADER;"
+
+psql $DATAFLOWS_DB_ENGINE -c "\copy (SELECT * FROM qc_classification) 
+                                TO '/home/db-facilities/output/qc_classification.csv' 
+                                DELIMITER ',' CSV HEADER;"
+
+psql $DATAFLOWS_DB_ENGINE -c "\copy (SELECT * FROM qc_captype) 
+                                TO '/home/db-facilities/output/qc_captype.csv' 
+                                DELIMITER ',' CSV HEADER;"
+
+psql $DATAFLOWS_DB_ENGINE -c "\copy (SELECT * FROM qc_proptype) 
+                                TO '/home/db-facilities/output/qc_proptype.csv' 
+                                DELIMITER ',' CSV HEADER;"
+
+psql $DATAFLOWS_DB_ENGINE -c "\copy (SELECT * FROM qc_mapped) 
+                                TO '/home/db-facilities/output/qc_mapped.csv' 
+                                DELIMITER ',' CSV HEADER;"
+echo "Build is done!"
