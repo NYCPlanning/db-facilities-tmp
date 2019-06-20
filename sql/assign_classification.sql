e36b82b6598637e9ed8c216a20569ce7838be248
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-06-20 18:07:23 -0400

    adding service area as a field to get mapped

--- sql/assign_classification.sql
+++ sql/assign_classification.sql
@@ -1,7 +1,8 @@
 -- assign the domain and group
 UPDATE facilities a
 SET facdomain = b.facdomain,
-	facgroup = b.facgroup
+	facgroup = b.facgroup,
+	servarea = b.servarea
 FROM facilities_classification b
 WHERE a.facsubgrp = b.facsubgrp;
 
