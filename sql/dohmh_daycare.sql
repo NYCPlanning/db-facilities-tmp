d8d71aae26b965cd00deea162943620215d7c3bb
Baoling Zhou <baolingz@nyu.edu>
2019-06-25 13:35:58 -0400

    update deduplicates

--- sql/dohmh_daycare.sql
+++ sql/dohmh_daycare.sql
@@ -2,6 +2,27 @@
 --from (select geo::json->'status' as status from dohmh_daycare) w
 --group by w.status::text;
 
+-- Update inspection_date with None value to '01/01/2000' temporarily
+UPDATE dohmh_daycare
+SET inspection_date = (CASE WHEN UPPER(inspection_date) = 'NONE' THEN '01/01/2000'
+							ELSE inspection_date
+						END);
+						
+-- Delete duplicates, only keep the records for each unique Day Care ID with the latest inspectation date
+DELETE FROM dohmh_daycare
+WHERE ogc_fid NOT IN(
+WITH date AS(
+SELECT day_care_id, MAX(inspection_date::date) as latest_date
+FROM dohmh_daycare
+GROUP BY day_care_id
+)
+SELECT min(ogc_fid) as orc_fid
+FROM dohmh_daycare dc, date d
+WHERE dc.day_care_id = d.day_care_id
+AND dc.inspection_date::date = d.latest_date
+GROUP BY dc.day_care_id
+);
+
 ALTER TABLE dohmh_daycare
 	ADD hash text, 
 	ADD	facname text,
@@ -22,6 +43,9 @@ ALTER TABLE dohmh_daycare
 
 update dohmh_daycare as t
 SET hash =  md5(CAST((t.*)AS text)), 
+	inspection_date = (CASE WHEN inspection_date::text = '01/01/2000' THEN 'None'
+								ELSE inspection_date::text
+							END),
 	facname = (CASE
 				WHEN center_name LIKE '%SBCC%' THEN initcap(legal_name)
 				WHEN center_name LIKE '%SCHOOL BASED CHILD CARE%' THEN initcap(legal_name)
