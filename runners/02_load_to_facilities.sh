c7068119994b6c566f533ab867c166f474991c9c
sptkl <caobaiyue@gmail.com>
2019-07-15 10:11:48 -0400

    add assign_xy.sql
    Former-commit-id: 9ac9c81dc9b5b0be9468939ccd73908e6b7ef209 [formerly 85dcef813476bae526ee154d9cb1d74f90dd9775]
    Former-commit-id: 02d36548d6be3e5e1a520b4423a7e8eec6621a70

--- runners/02_load_to_facilities.sh
+++ runners/02_load_to_facilities.sh
@@ -3,4 +3,5 @@ psql $DATAFLOWS_DB_ENGINE -f sql/assign_classification.sql
 psql $DATAFLOWS_DB_ENGINE -f sql/assign_overlevel.sql
 psql $DATAFLOWS_DB_ENGINE -f sql/assign_overlevel.sql
 psql $DATAFLOWS_DB_ENGINE -f sql/assign_bin_centroid.sql
+psql $DATAFLOWS_DB_ENGINE -f sql/assign_xy.sql
 psql $DATAFLOWS_DB_ENGINE -f sql/qc_views.sql
---
+++ sql/assign_xy.sql
@@ -0,0 +1,4 @@
+UPDATE facilities
+SET xcoord = ST_X(ST_TRANSFORM(geom, 2263)),
+    ycoord = ST_Y(ST_TRANSFORM(geom, 2263))
+;
