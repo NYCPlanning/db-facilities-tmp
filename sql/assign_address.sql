27f0baab6f11c45b07c3ae3cc27c72c7e8f9ae1f
sptkl <caobaiyue@gmail.com>
2019-07-17 14:09:15 -0400

    remove address spacing

--- sql/assign_address.sql
+++ sql/assign_address.sql
@@ -1,3 +1,7 @@
 UPDATE facilities
-SET address = UPPER(address)
+SET address = UPPER(address);
+
+UPDATE facilities
+SET address = trim(regexp_replace(address, E'[ tnr]+', ' ', 'g')),
+	streetname = trim(regexp_replace(streetname, E'[ tnr]+', ' ', 'g'))
 ;
