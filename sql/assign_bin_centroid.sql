6c9e39b171eb5155bac895815afb9ca79885d169
sptkl <caobaiyue@gmail.com>
2019-07-17 16:40:23 +0000

    simplify bin centroid [skip ci]
    Former-commit-id: 6b90ccb4d36623e86d8d3a6088dd62f88936e1d4 [formerly c063765ec0a5aecd447ea872080d56ca8ad14802]
    Former-commit-id: 1dc0962afc03c88e0a0cd34e4a691c12e9780c9f

--- sql/assign_bin_centroid.sql
+++ sql/assign_bin_centroid.sql
@@ -1,11 +1,7 @@
 /** Reassign geometry attributes based on bin centroids for records
 having bin and bbl combination existing in doitt_buildingcentroids **/
 UPDATE facilities f
-SET geom = b.wkb_geometry,
-	latitude = ST_Y(b.wkb_geometry)::text,
-	longitude = ST_X(b.wkb_geometry)::text,
-	xcoord = ST_X(ST_TRANSFORM(wkb_geometry, 2263)),
-	ycoord = ST_Y(ST_TRANSFORM(wkb_geometry, 2263))
+SET geom = b.wkb_geometry
 FROM doitt_buildingcentroids b
 WHERE f.bin = b.bin
 AND f.bbl = b.base_bbl;
