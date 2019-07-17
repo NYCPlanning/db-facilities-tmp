81debabfe46642405ebdd7eba69fac9769d4579f
sptkl <caobaiyue@gmail.com>
2019-07-17 16:08:55 +0000

    backfill lat lon [skip ci]

--- sql/assign_xy.sql
+++ sql/assign_xy.sql
@@ -1,4 +1,12 @@
 UPDATE facilities
 SET xcoord = ST_X(ST_TRANSFORM(geom, 2263)),
-    ycoord = ST_Y(ST_TRANSFORM(geom, 2263))
+    ycoord = ST_Y(ST_TRANSFORM(geom, 2263)),
+    longitude = (CASE
+                    WHEN longitude IS NULL THEN ST_X(geom)::text
+                    ELSE longitude
+                END),
+    latitude = (CASE
+                    WHEN latitude IS NULL THEN ST_Y(geom)::text
+                    ELSE latitude
+                END)
 ;
