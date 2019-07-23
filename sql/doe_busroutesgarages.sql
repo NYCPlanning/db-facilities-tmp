12d0ab0b60d0352c9b941627f27416c76f45c224
Baoling Zhou <baolingz@nyu.edu>
2019-07-23 15:18:34 -0400

    Revert "deduplicate for doe_busroutesgarages"
    This reverts commit c4296bed1629df31846fa3c8735acc3bfe77d8e1.

--- sql/doe_busroutesgarages.sql
+++ sql/doe_busroutesgarages.sql
@@ -20,46 +20,26 @@ ALTER TABLE doe_busroutesgarages
 	ADD	captype text,
 	ADD	proptype text;
 
-WITH capacity AS(
-	SELECT vendor_name, garage__street_address, COUNT(DISTINCT(route_number)) AS route_counts 
-	FROM doe_busroutesgarages
-	GROUP BY vendor_name, garage__street_address
-	)
 update doe_busroutesgarages as t
 SET hash =  md5(CAST((t.*)AS text)),
 	address = (CASE 
 	                    WHEN geo_street_name is not NULL and geo_house_number is not NULL 
 	                        THEN geo_house_number || ' ' || geo_street_name
-	                    ELSE t.garage__street_address         
+	                    ELSE garage__street_address         
 	                END),
-	facname = initcap(t.vendor_name),
+	facname = initcap(vendor_name),
 	factype = 'School Bus Depot',
 	facsubgrp = 'Bus Depots and Terminals',
 	facgroup = NULL,
 	facdomain = NULL,
 	servarea = NULL,
-	opname = initcap(t.vendor_name),
+	opname = initcap(vendor_name),
 	opabbrev = 'Non-public',
 	optype = 'Non-public',
 	overagency = 'NYC Department of Education',
 	overabbrev = 'NYCDOE', 
 	overlevel = NULL, 
-	capacity = route_counts, 
-	captype = 'routes', 
+	capacity = NULL, 
+	captype = NULL, 
 	proptype = NULL
-FROM capacity c
-WHERE t.vendor_name = c.vendor_name
-AND t.garage__street_address = c.garage__street_address
 ;
-
--- Depulicate by vender_name and garage__street_address
-WITH tmp AS(
-SELECT MIN(ogc_fid) AS ogc_fid
-FROM doe_busroutesgarages
-GROUP BY vendor_name, garage__street_address
-)
-DELETE FROM doe_busroutesgarages
-WHERE ogc_fid NOT IN (
-	SELECT ogc_fid
-	FROM tmp
-	);
