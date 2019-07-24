f7698237ff9ab0746c1ed4bc655c2f5d86ddf061
sptkl <caobaiyue@gmail.com>
2019-07-24 14:41:37 +0000

    remove duplicated assign overlevel [skip ci]
    Former-commit-id: 5d7333959f10bd3f49a515c68a4ae207110e93ea [formerly 869916e4f2188615077da09302a29380973e012b]
    Former-commit-id: 5903b9a119aae8d3f647e604af1209655478022b

--- runners/02_load_to_facilities.sh
+++ runners/02_load_to_facilities.sh
@@ -1,4 +1,3 @@
 psql $DATAFLOWS_DB_ENGINE -f sql/load_and_combine.sql
 psql $DATAFLOWS_DB_ENGINE -f sql/assign_classification.sql
 psql $DATAFLOWS_DB_ENGINE -f sql/assign_overlevel.sql
-psql $DATAFLOWS_DB_ENGINE -f sql/assign_overlevel.sql
