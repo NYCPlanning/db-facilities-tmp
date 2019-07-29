c4e751d61dece6692c90dff25ed5f8713edce0f6
sptkl <caobaiyue@gmail.com>
2019-07-29 15:02:47 +0000

    apply small fixes [skip ci]
    Former-commit-id: 007903a75a0bd01f3938d8efd9eb041401b34477 [formerly 8da9a91d2af07bb4b36f0ee638ddaa2a13ec2d11]
    Former-commit-id: 55fe54f1b20c26be8c47c247db207984eb38e0ef

--- sql/doe_lcgms.sql
+++ sql/doe_lcgms.sql
@@ -38,75 +38,63 @@ CREATE TABLE doe_lcgms AS (SELECT * FROM doe_lcgms_tmp);
 DROP TABLE doe_lcgms_tmp;
 
 
-update doe_lcgms as t
+UPDATE doe_lcgms as t
 SET hash =  md5(CAST((t.*)AS text)),
-	wkb_geometry = (CASE
-				        WHEN wkb_geometry IS NULL
-					        THEN ST_SetSRID(ST_Point(longitude::DOUBLE PRECISION, 
-												 	 latitude::DOUBLE PRECISION), 4326)
-				        ELSE wkb_geometry
-				    END),
-	address = (CASE 
-                    WHEN geo_street_name is not NULL and geo_house_number is not NULL 
-                        THEN geo_house_number || ' ' || geo_street_name
-                    ELSE primary_address         
-                END),
-	facname = location_name,
-	factype = (CASE
-				
-				WHEN managed_by_name = 'Charter' AND location_category_description ~* '%school%' THEN 
-					CASE 
-						WHEN location_type_description NOT LIKE '%Special%' THEN CONCAT(location_category_description, ' - Charter')
-						WHEN location_type_description LIKE '%Special%' THEN CONCAT(location_category_description, ' - Charter, Special Education')
-					END
-					
-				WHEN managed_by_name = 'Charter' THEN 
-					CASE 
-						WHEN location_type_description NOT LIKE '%Special%' THEN CONCAT(location_category_description, ' School - Charter')
-						WHEN location_type_description LIKE '%Special%' THEN CONCAT(location_category_description, ' School - Charter, Special Education')
-					END
-	
-				WHEN location_category_description ~* '%school%' THEN
-					CASE
-						WHEN location_type_description NOT LIKE '%Special%' THEN CONCAT(location_category_description, ' - Public')
-						WHEN location_type_description LIKE '%Special%' THEN CONCAT(location_category_description, ' - Public, Special Education')
-					END
-	
-				WHEN location_type_description ~* '%Special%' THEN CONCAT(location_category_description, ' School - Public, Special Education')
-	
-				ELSE CONCAT(location_category_description, ' School - Public')
-				
-			END),
-	facsubgrp = (CASE
-
-					WHEN location_type_description LIKE '%Special%' THEN 'Public and Private Special Education Schools'
-		
-					WHEN location_category_description LIKE '%Early%' OR location_category_description LIKE '%Pre-K%' THEN 'DOE Universal Pre-Kindergarten'
-		
-					WHEN managed_by_name = 'Charter' THEN 'Charter K-12 Schools'
-		
-					ELSE 'Public K-12 Schools'
-		
+wkb_geometry = (CASE
+				WHEN wkb_geometry IS NULL
+				THEN ST_SetSRID(ST_Point(longitude::DOUBLE PRECISION,latitude::DOUBLE PRECISION), 4326)
+				ELSE wkb_geometry
 				END),
-	facgroup = NULL,
-	facdomain = NULL,
-	servarea = NULL,
-	opname = (CASE
-						WHEN managed_by_name = 'Charter' THEN location_name
-						ELSE 'NYC Department of Education'
-					END),
-	opabbrev = (CASE
-						WHEN managed_by_name = 'Charter' THEN 'Non-public'
-						ELSE 'NYCDOE'
-					END),
-	optype = (CASE
-						WHEN managed_by_name = 'Charter' THEN 'Non-public'
-						ELSE 'Public'
-					END),
-	overagency = 'NYC Department of Education',
-	overabbrev = 'NYCDOE', 
-	overlevel = NULL, 
-	capacity = target_capacity, 
-	captype = 'seats', 
-	proptype = NULL
+address = (CASE 
+				WHEN geo_street_name is not NULL and geo_house_number is not NULL 
+				THEN geo_house_number || ' ' || geo_street_name
+				ELSE primary_address         
+				END),
+facname = location_name,
+factype = (CASE
+				WHEN managed_by_name = 'Charter' AND location_category_description ~* '%school%' THEN 
+				CASE 
+				WHEN location_type_description NOT LIKE '%Special%' THEN CONCAT(location_category_description, ' - Charter')
+				WHEN location_type_description LIKE '%Special%' THEN CONCAT(location_category_description, ' - Charter, Special Education')
+				END
+				WHEN managed_by_name = 'Charter' THEN 
+				CASE 
+				WHEN location_type_description NOT LIKE '%Special%' THEN CONCAT(trim(regexp_replace(location_category_description, 'school|School', '')), ' School - Charter')
+				WHEN location_type_description LIKE '%Special%' THEN CONCAT(location_category_description, ' School - Charter, Special Education')
+				END
+				WHEN location_category_description ~* '%school%' THEN
+				CASE
+				WHEN location_type_description NOT LIKE '%Special%' THEN CONCAT(location_category_description, ' - Public')
+				WHEN location_type_description LIKE '%Special%' THEN CONCAT(location_category_description, ' - Public, Special Education')
+				END
+				WHEN location_type_description ~* '%Special%' THEN CONCAT(trim(regexp_replace(location_category_description, 'school|School', '')), ' School - Public, Special Education')
+				ELSE CONCAT(trim(regexp_replace(location_category_description, 'school|School', '')), ' School - Public')
+				END),
+facsubgrp = (CASE
+				WHEN location_type_description LIKE '%Special%' THEN 'Public and Private Special Education Schools'
+				WHEN location_category_description LIKE '%Early%' OR location_category_description LIKE '%Pre-K%' THEN 'DOE Universal Pre-Kindergarten'
+				WHEN managed_by_name = 'Charter' THEN 'Charter K-12 Schools'
+				ELSE 'Public K-12 Schools'
+				END),
+facgroup = NULL,
+facdomain = NULL,
+servarea = NULL,
+opname = (CASE
+			WHEN managed_by_name = 'Charter' THEN location_name
+			ELSE 'NYC Department of Education'
+			END),
+opabbrev = (CASE
+			WHEN managed_by_name = 'Charter' THEN 'Non-public'
+			ELSE 'NYCDOE'
+			END),
+optype = (CASE
+			WHEN managed_by_name = 'Charter' THEN 'Non-public'
+			ELSE 'Public'
+			END),
+overagency = 'NYC Department of Education',
+overabbrev = 'NYCDOE',
+overlevel = NULL,
+capacity = target_capacity,
+captype = 'seats',
+proptype = NULL
 ;
