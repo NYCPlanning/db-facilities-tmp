76226084d9133d4c03eae26fd5b2e281eee4b674
sptkl <caobaiyue@gmail.com>
2019-07-29 19:27:59 +0000

    rerun [skip ci]
    Former-commit-id: 9cadb972f5f2dc4a9711543fc1967fa95d5a301a [formerly c32fbd3c235f9044c650561780d8c11296d4fc76]
    Former-commit-id: 3a9d06f6d753d747e57f3f570747b3ba30859060

--- sql/nycourts_courts.sql
+++ sql/nycourts_courts.sql
@@ -28,7 +28,7 @@ SET hash = md5(CAST((t.*)AS text)),
                         ELSE address             
                     END),
 	facname = name,
-	factype = 'Court House',
+	factype = 'Courthouse',
 	facsubgrp = 'Courthouses and Judicial',
 	facgroup = NULL,
 	facdomain = NULL,
@@ -41,4 +41,4 @@ SET hash = md5(CAST((t.*)AS text)),
 	overlevel = NULL,
 	capacity = NULL,
 	captype = NULL,
-	proptype = NULL
+	proptype = NULL;
