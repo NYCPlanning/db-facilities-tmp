690db6696a2f9932588fca568d0cdfebb19793f3
sptkl <caobaiyue@gmail.com>
2019-07-31 18:04:39 +0000

    update [skip ci]
    Former-commit-id: 94eab95b6230ce82a4e660ccaf1d9af6ccd60d02 [formerly 334f0f6b1e291b23cf37a0665f65bb6e0543a44e]
    Former-commit-id: 9b8542c4b67d67d752f3b4d0479f648b911cc524

--- sql/formatting.sql
+++ sql/formatting.sql
@@ -9,3 +9,13 @@ SET facname = UPPER(facname);
 -- remove community board offices from dcp_sfpsd
 DELETE from facilities
 WHERE datasource = 'dcp_sfpsd' and facname ~* 'COMM BD';
+
+UPDATE facilities a
+SET factype =   REPLACE(
+                REPLACE(
+                REPLACE(
+                REPLACE(factype, 
+                    'Usda Community Eligibility Option', 'USDA Community Eligibility Option'),
+                    'Ged-Alternative High School Equivalency Prep Programs', 'GED-Alternative High School Equivalency Prep Programs'),
+                    'Registered Esl Schools', 'Registered ESL Schools'), 
+                    'Diagnostic & Treatment Center', 'Diagnostic and Treatment Center');
