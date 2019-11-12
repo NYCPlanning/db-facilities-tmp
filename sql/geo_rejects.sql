fc62858fa791d5e44c7c4c6f497801ba26adcb19
Baoling Zhou <baolingz@nyu.edu>
2019-11-12 22:17:05 +0000

    update geo_rejects table [skip ci]
    Former-commit-id: 996e91aa3d385f188d17950e49d0213fe0e24b82 [formerly 719db21521550ecef0483adefaf0b3da908f889e]
    Former-commit-id: b370898a2ba540cda99af6c4d7914d2f824d1b28

--- sql/geo_rejects.sql
+++ sql/geo_rejects.sql
@@ -1,8 +1,11 @@
 -- Output the records not having geoms
 DROP TABLE IF EXISTS geo_rejects;
-SELECT * INTO geo_rejects
+SELECT facname, addressnum, streetname,
+address, datasource, uid, grc, grc2
+INTO geo_rejects
 FROM facilities
-WHERE geom IS NULL;
+WHERE geom IS NULL
+ORDER BY datasource, facname ASC;
 
 ALTER TABLE facilities
 DROP column grc,
