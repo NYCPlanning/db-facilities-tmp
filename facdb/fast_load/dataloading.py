4c366217c56b9be8b1c207c655392e9927f2d207
Baoling Zhou <baolingz@nyu.edu>
2019-11-14 21:21:34 +0000

    update dcp_pops [skip ci]
    Former-commit-id: 489db65c08748ae06ed2991ad052e6d62150abe1 [formerly f7638d8b31bc1c66a8806f8bd7c73685aa1a399e]
    Former-commit-id: 54ff3e504f610a93af5ad9c1c8d583428e1edf25

--- facdb/fast_load/dataloading.py
+++ facdb/fast_load/dataloading.py
@@ -5,8 +5,8 @@ def ETL():
     RECIPE_ENGINE = os.environ.get('FACDB_ENGINE', '')
     BUILD_ENGINE=os.environ.get('BUILD_ENGINE', '')
 
+    #load geoboundaries
     importer = Importer(RECIPE_ENGINE, BUILD_ENGINE)
-    importer.import_table(schema_name='dcp_facilities')
     importer.import_table(schema_name='dcp_boroboundaries_wi')
     importer.import_table(schema_name='dcp_censustracts')
     importer.import_table(schema_name='dcp_councildistricts')
@@ -16,16 +16,22 @@ def ETL():
     importer.import_table(schema_name='doitt_zipcodeboundaries')
     importer.import_table(schema_name='doitt_buildingcentroids')
     importer.import_table(schema_name='dcp_school_districts')
-    importer.import_table(schema_name='nysdec_lands')
-    importer.import_table(schema_name='nysparks_parks')
-    importer.import_table(schema_name='nysparks_historicplaces')
-    importer.import_table(schema_name='dpr_parksproperties')
-    importer.import_table(schema_name='usdot_ports')
-    importer.import_table(schema_name='usnps_parks')
+
+    #load reference table
+    importer.import_table(schema_name='dcp_facilities')
     importer.import_table(schema_name='facilities_classification')
     importer.import_table(schema_name='dcp_mappluto')
     importer.import_table(schema_name='zipcode_city_lookup')
+
+    #load facilities
+    importer.import_table(schema_name='dcp_pops')
+    importer.import_table(schema_name='dpr_parksproperties')
     importer.import_table(schema_name='moeo_socialservicesiteloactions')
+    importer.import_table(schema_name='nysdec_lands')
+    importer.import_table(schema_name='nysparks_historicplaces')
+    importer.import_table(schema_name='nysparks_parks')
+    importer.import_table(schema_name='usdot_ports')
+    importer.import_table(schema_name='usnps_parks')
 
 if __name__ == "__main__":
     ETL()
