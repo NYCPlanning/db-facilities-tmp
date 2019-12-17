b19b90ef1b59c75dba63039a0714805efdfb3c10
Baoling Zhou <baolingz@nyu.edu>
2019-12-17 18:05:23 +0000

    update moeo and dcp_pops [skip ci]
    Former-commit-id: 819feb02fbf70d4e7849bedff6e1bea6297d8119 [formerly 7760d2816864e973b77763004681c963c3598ba1]
    Former-commit-id: 5a64786ace39acceae4e11f4ec74639df58389ff

--- etl.sh
+++ etl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source config.sh
-NAME=nycdoc_corrections
+NAME=moeo_socialservicesiteloactions
 
 docker exec $ETL_CONTAINER_NAME python facdb/recipes/$NAME.py
 docker exec $DB_CONTAINER_NAME psql -h localhost -U postgres -f sql/$NAME.sql
