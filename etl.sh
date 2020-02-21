7a34c691b4fb91434006db37d0b43126205e43e1
sptkl <caobaiyue@gmail.com>
2020-02-21 19:59:10 +0000

    fixing ci, remove circleci and zip shapefiles

--- etl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source config.sh
-NAME=doe_busroutesgarages
-
-docker exec $ETL_CONTAINER_NAME python facdb/recipes/$NAME.py
-docker exec $DB_CONTAINER_NAME psql -h localhost -U postgres -f sql/$NAME.sql
