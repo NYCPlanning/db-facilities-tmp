4363fb82a6a66094cd51781568aa47c6ca15aa85
sptkl <caobaiyue@gmail.com>
2020-03-24 13:41:30 +0000

    changing workflow file [build]

--- facdb/fast_load/dataloading.py
+++ facdb/fast_load/dataloading.py
@@ -3,44 +3,9 @@ from multiprocessing import Pool, cpu_count
 import os
 
 source_tables = [
-    'sca_bluebook',
-    'dot_bridgehouses',
-    'dot_ferryterminals',
-    'dot_mannedfacilities',
-    'dot_pedplazas',
-    'dot_publicparking',
-    'dpr_parksproperties',
-    'dsny_mtsgaragemaintenance',
-    'dycd_afterschoolprograms',
-    'fbop_corrections',
-    'fdny_firehouses',
-    'foodbankny_foodbanks',
-    'hhc_hospitals',
-    'hra_centers',
-    'moeo_socialservicesiteloactions',
-    'nycdoc_corrections',
-    'nycha_communitycenters',
-    'nycha_policeservice',
-    'nycourts_courts',
-    'nypl_libraries',
     'nysdec_lands',
-    'nysdec_solidwaste',
-    'nysdoccs_corrections',
-    'nysdoh_healthfacilities',
-    'nysdoh_nursinghomes',
-    'nysed_activeinstitutions',
-    'nysed_nonpublicenrollment',
-    'nysoasas_programs',
-    'nysomh_mentalhealth',
-    'nysopwdd_providers',
     'nysparks_historicplaces',
-    'nysparks_parks',
-    'qpl_libraries',
-    'sbs_workforce1',
-    'uscourts_courts',
-    'usdot_airports',
-    'usdot_ports',
-    'usnps_parks'
+    'nysparks_parks'
 ]
 
 boundry_tables = [
