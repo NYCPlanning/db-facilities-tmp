00dce3d32a1644f4f6d77cfaa57728fca62b8be3
sptkl <caobaiyue@gmail.com>
2020-03-24 18:27:11 +0000

    [build] fix foodbank typo

--- sql/foodbankny_foodbanks.sql
+++ sql/foodbankny_foodbanks.sql
@@ -31,10 +31,10 @@ SET hash =  md5(CAST((t.*)AS text)),
 	factype = (CASE
 				WHEN program_type ~* 'pantry' THEN 'Food Pantry'
 				WHEN program_type ~* 'Soup Kitchen' THEN 'Soup Kitchen'
-				when program_tupe ~* 'senior' THEN 'Senior Center'
+				when program_type ~* 'senior' THEN 'Senior Center'
 			  END),
 	facsubgrp = (CASE
-				when program_tupe ~* 'senior' THEN 'Senior Services'
+				when program_type ~* 'senior' THEN 'Senior Services'
 				else 'Soup Kitchens and Food Pantries'
 			  END),
 	facgroup = NULL,
