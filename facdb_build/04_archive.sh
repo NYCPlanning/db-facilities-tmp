8b29fd78f8b252bd2992d235f212fbbe9f392938
sptkl <caobaiyue@gmail.com>
2020-06-24 17:38:03 +0000

    [build] fix archive

--- facdb_build/04_archive.sh
+++ facdb_build/04_archive.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source config.sh
 
-pg_dump $BUILD_ENGINE | psql $EDM_DATA
+pg_dump $BUILD_ENGINE -t facilities -O | psql $EDM_DATA
 DATE=$(date "+%Y/%m/%d");
 psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS facilities;";
 psql $EDM_DATA -c "ALTER TABLE facilities SET SCHEMA facilities;";
 psql $EDM_DATA -c "DROP TABLE IF EXISTS facilities.\"$DATE\";";
 psql $EDM_DATA -c "ALTER TABLE facilities.facilities RENAME TO \"$DATE\";";
-psql $EDM_DATA -c "DROP TABLE IF EXISTS facilities.latest;";
-psql $EDM_DATA -c "SELECT * INTO facilities.latest FROM facilities.\"$DATE\";";
+psql $EDM_DATA -c "DROP VIEW IF EXISTS facilities.latest;";
+psql $EDM_DATA -c "CREATE VIEW facilities.latest AS (SELECT '$DATE' as v, * FROM facilities.\"$DATE\");";
