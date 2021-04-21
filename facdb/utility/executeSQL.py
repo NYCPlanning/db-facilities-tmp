fd5303467abcec0f4a7cd0f4355339492a96a497
sptkl <caobaiyue@gmail.com>
2021-04-21 15:13:59 +0000

    make sure psql thorws non 0 return if there is an error

--- facdb/utility/executeSQL.py
+++ facdb/utility/executeSQL.py
@@ -4,5 +4,5 @@ from . import BUILD_ENGINE
 
 
 def ExecuteSQL(script):
-    os.system(f"""psql {BUILD_ENGINE} -f {script}""")
+    os.system(f"""psql {BUILD_ENGINE} -v ON_ERROR_STOP=1 -f {script}""")
     return None
