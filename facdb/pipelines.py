9b1b57166d181134ab370f93e4a6e7a4c5c2f525
sptkl <caobaiyue@gmail.com>
2021-04-23 17:50:17 +0000

    adding facdb/sql/nysed_activeinstitutions.sql

--- facdb/pipelines.py
+++ facdb/pipelines.py
@@ -492,6 +492,12 @@ def nysdoh_nursinghomes(df: pd.DataFrame = None):
 
 
 @Export
+@Prepare
+def nysed_nonpublicenrollment(df: pd.DataFrame = None):
+    return df
+
+
+@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
