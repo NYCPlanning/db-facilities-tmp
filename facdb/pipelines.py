7263c27a4d0bfa2121ff82ebb5693bdec73ee2ff
sptkl <caobaiyue@gmail.com>
2021-04-26 16:33:23 +0000

    hard code school year to be 2019-2020 for doe bus routes

--- facdb/pipelines.py
+++ facdb/pipelines.py
@@ -142,7 +142,8 @@ def dfta_contracts(df: pd.DataFrame = None):
 @ParseAddress(raw_address_field="garage__street_address")
 @Prepare
 def doe_busroutesgarages(df: pd.DataFrame = None):
-    SCHOOL_YEAR = f"{datetime.date.today().year-1}-{datetime.date.today().year}"
+    # SCHOOL_YEAR = f"{datetime.date.today().year-1}-{datetime.date.today().year}"
+    SCHOOL_YEAR = "2019-2020"  # No records from 2020-2021 school year
     df = df.loc[df.school_year == SCHOOL_YEAR, :]
     return df
 
