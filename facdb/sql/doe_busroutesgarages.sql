13238ccfeb8d663b4248d1828b475c8d76a8d2c5
mgraber <mgraber@planning.nyc.gov>
2021-04-27 13:00:44 +0000

    Add route count and deduplication

--- facdb/sql/doe_busroutesgarages.sql
+++ facdb/sql/doe_busroutesgarages.sql
@@ -1,30 +1,45 @@
-DROP TABLE IF EXISTS _doe_busroutesgarages;
+--DROP TABLE IF EXISTS _doe_busroutesgarages;
+WITH
+capacity AS(
+	SELECT vendor_name, garage__street_address, COUNT(DISTINCT(route_number)) AS route_counts
+	FROM doe_busroutesgarages
+	GROUP BY vendor_name, garage__street_address
+),
+min_records AS (
+    SELECT MIN(uid) AS min_uid
+	FROM doe_busroutesgarages
+	GROUP BY vendor_name, garage__street_address
+)
 SELECT
-    uid,
-    source,
-    initcap(vendor_name) as facname,
-    parsed_hnum as addressnum,
-    parsed_sname as streetname,
-    garage__street_address as address,
-    garage_city as city,
-    garage_zip as zipcode,
-    garage_city as boro,
+    a.uid,
+    a.source,
+    initcap(a.vendor_name) as facname,
+    a.parsed_hnum as addressnum,
+    a.parsed_sname as streetname,
+    a.garage__street_address as address,
+    a.garage_city as city,
+    a.garage_zip as zipcode,
+    a.garage_city as boro,
     NULL as borocode,
     NULL as bin,
     NULL as bbl,
     'School Bus Depot' as factype,
     'Bus Depots and Terminals' as facsubgrp,
-    initcap(vendor_name) as opname,
+    initcap(a.vendor_name) as opname,
     'Non-public' as opabbrev,
     'NYCDOE' as overabbrev,
-    NULL as capacity,
+    b.route_counts as capacity,
     'routes' as captype,
     NULL as proptype,
-    wkt::geometry as wkb_geometry,
-    geo_1b,
+    a.wkt::geometry as wkb_geometry,
+    a.geo_1b,
     NULL as geo_bl,
     NULL as geo_bn
-INTO _doe_busroutesgarages
-FROM doe_busroutesgarages;
+--INTO _doe_busroutesgarages
+FROM doe_busroutesgarages a
+LEFT JOIN capacity b
+ON a.vendor_name = b.vendor_name
+AND a.garage__street_address = b.garage__street_address
+WHERE a.uid IN (SELECT min_uid FROM min_records);
 
-CALL append_to_facdb_base('_doe_busroutesgarages');
+--CALL append_to_facdb_base('_doe_busroutesgarages');
