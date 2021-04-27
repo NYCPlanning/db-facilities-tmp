10eedef524fc1d810569633f42db29eed4ff6dc7
mgraber <mgraber@planning.nyc.gov>
2021-04-27 17:56:51 +0000

    Add dycd_afterschoolprograms.sql

---
+++ facdb/sql/dycd_afterschoolprograms.sql
@@ -0,0 +1,41 @@
+--DROP TABLE IF EXISTS _dycd_afterschoolprograms;
+
+SELECT
+    uid,
+    source,
+    site_name as facname,
+    parsed_hnum as addressnum,
+    parsed_sname as streetname,
+    location_1 as address,
+    NULL as city,
+    postcode as zipcode,
+    SPLIT_PART(borough__community, ',', 1) as boro,
+    NULL as borocode,
+    bin,
+    bbl,
+    REPLACE(program, 'NDA Immigrats', 'NDA Immigrants')||' '||program_type as factype,
+    (CASE
+		WHEN program = 'Beacon'
+			OR program = 'Beacon Satellite'
+			OR program = 'High-School Aged Youth'
+			OR program = 'Middle School Youth'
+			OR program = 'Teen Action Program'
+		    THEN 'After-School Programs'
+		WHEN program_type ~* 'Immigrant Support Services'
+			THEN 'Immigrant Services'
+			ELSE 'Youth Centers, Literacy Programs, and Job Training Services'
+	END) as facsubgrp,
+    agency as opname,
+    NULL as opabbrev,
+    'NYCDYCD' as overabbrev,
+    NULL as capacity,
+    NULL as captype,
+    NULL as proptype,
+    NULL as wkb_geometry,
+    geo_1b,
+    geo_bl,
+    geo_bn
+--INTO _dycd_afterschoolprograms
+FROM dycd_afterschoolprograms;
+
+--CALL append_to_facdb_base('_dycd_afterschoolprograms');
